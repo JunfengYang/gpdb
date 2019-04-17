435e762e98112ffc717c70e14068c10f3ba9f69e
Junfeng(Jerome) Yang <jeyang@pivotal.io>
2019-04-17 13:19:08 +0800

    zombo work

--- src/backend/catalog/index.c
+++ src/backend/catalog/index.c
@@ -2886,8 +2886,8 @@ IndexBuildHeapScan(Relation heapRelation,
 		else
 		{
 			/* Call the AM's callback routine to process the tuple */
-			//callback(indexRelation, &heapTuple->t_self, values, isnull, tupleIsAlive,
-			callback(indexRelation, heapTuple, values, isnull, tupleIsAlive,
+			//callback(indexRelation, heapTuple, values, isnull, tupleIsAlive,
+			callback(indexRelation, &heapTuple->t_self, values, isnull, tupleIsAlive,
 					 callback_state);
 		}
 	}
